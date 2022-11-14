e56c4614ee53dc255fb4fdd1e21a95896bbfbdb6
harshalgr8 <harshal.dawange@gmail.com>
2022-11-14 14:27:20 +0530

    initial changes

---
+++ Students/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem.Students
+{
+    internal interface IStudent
+    {
+        internal void GetStudents();
+        internal void SaveStudent();
+       
+        internal void DeleteStudent();
+    }
+}
