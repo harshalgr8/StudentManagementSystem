d6a8a0f1035bfcce85fec9331390a9224040cfe3
harshalgr8 <harshal.dawange@gmail.com>
2022-11-14 14:27:20 +0530

    initial changes

---
+++ Students/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem.Students
+{
+    internal class Student : IStudent
+    {
+        void IStudent.DeleteStudent()
+        {
+           
+        }
+
+        void IStudent.GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IStudent.SaveStudent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
