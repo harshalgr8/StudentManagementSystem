ca0e0a2c12536fdb721add315407b64f732aa511
harshalgr8 <harshal.dawange@gmail.com>
2022-11-14 14:29:50 +0530

    added standard

---
+++ Students/IStandard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem.Students
+{
+    internal interface IStandard
+    {
+        internal void GetStandards();
+        internal void LoadStandards();
+        internal void SaveStandards();
+
+    }
+}
