7866cc9cc0aba138b27e163ec6c87ccb0608805e
harshalgr8 <harshal.dawange@gmail.com>
2022-11-14 14:50:16 +0530

    added student

---
+++ Students/IStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem.Students
+{
+    internal interface IStudent
+    {
+        internal void addStudent();
+        internal void removeStudent();
+        internal void saveStudent();
+    }
+}
---
+++ Students/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem.Students
+{
+    internal class Student : IStudent
+    {
+        void IStudent.addStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IStudent.removeStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IStudent.saveStudent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
